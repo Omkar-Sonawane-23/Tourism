7d098760e7ba667e592b359e01a0b537154ddde6
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-05-10 16:28:32 +0530

    updating places card ui

--- src/components/Homeque.jsx
+++ src/components/Homeque.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import Data from "../../Data/Detailed.json";
 
 const Homeque = () => {
@@ -10,20 +10,36 @@ const Homeque = () => {
   };
 
   return (
-    <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 gap-y-2 justify-center items-center'>
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 gap-y-2 justify-center items-center">
       {data.map((ele, i) => (
-        <div key={i} className='w-full sm:w-[20rem] h-[25rem] m-[4rem]  relative flex flex-col justify-center items-center'>
-          <div className='w-full h-[30rem] overflow-hidden rounded-[30px]'>
-            <img src={data[i].image} className='rounded-[30px] w-full h-[30rem] bg-cover' alt="Heritage Image" />
+        <article className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto mt-24 bg-gray-900/40 shadow-lg hover:shadow-2xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:bg-gray-900/60"
+         key={i}>
+          <img
+            src={data[i].image}
+            alt=""
+            className="absolute inset-0 h-full w-full object-cover"
+          />
+          <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+          <h3 className="z-10 mt-3 text-4xl  font-bold text-white text-center">
+            {data[i].name}
+          </h3>
+          <div className="z-10 gap-y-1 pt-4 overflow-hidden text-center text-sm leading-6 text-gray-300">
+            {data[i].description}
           </div>
-          <div className='w-full h-[20rem] bg-[black] mt-[-30rem] z-[2] opacity-[30%] rounded-[30px] h-[20rem] flex flex-col justify-center items-center'>
-            <h1 className='text-[4rem] mt-[0px] text-center leading-[60px] z-[2] text-[white]'>{data[i].name}</h1>
-            <p className='text-[white] z-[2] w-[18rem]'>{data[i].description}</p>
-          </div>
+
+          <div className="z-10 mt-4 justify-center text-center items-center">
           <Link to={`/detail/?id=${i}`} key={i}>
-            <button onClick={() => showdetail(i)} className='w-[80px] h-[30px] border-2 rounded-xl text-[black] backdrop-blur hover:text-[1.25rem] cursor-pointer text-[white] hover:w-[100px] hover:h-[50px] duration-[0.4s] mt-[5rem]'>Explore</button>
+            <button
+              onClick={() => showdetail(i)}
+              className="w-[80px] h-[30px]  border-2 rounded-xl text-[black] backdrop-blur hover:text-[1.25rem] cursor-pointer text-[white] hover:w-[100px] hover:h-[50px] duration-[0.4s] mt-[5rem]
+              "
+            >
+              Explore
+            </button>
           </Link>
-        </div>
+          </div>
+
+        </article>
       ))}
     </section>
   );
