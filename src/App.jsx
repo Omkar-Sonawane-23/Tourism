24d0c2f2045379419d7ea8cbbf90696be9dcff23
Your Name <you@example.com>
2024-05-11 10:42:12 +0530

    darkmode added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
+// App.js
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import Explore from './pages/Explore'
+import Explore from './pages/Explore';
 import Homeque from './components/Homeque';
 import DetailView from './components/DetailView';
-import "./App.css"
+import "./App.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(prevMode => !prevMode);
+  };
+
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Routes>
-        <Route path="/home" element={<Homeque />} />
-        <Route path='/explore' element={<Explore/>}/>
-        <Route path="/detail" element={<DetailView />} /> {/* Corrected the placement of element prop */}
-        <Route path='/' element={<Home/>}/>
+        <Route path="/home" element={<Homeque darkMode={darkMode} />} />
+        <Route path='/explore' element={<Explore darkMode={darkMode} />} />
+        <Route path="/detail" element={<DetailView darkMode={darkMode} />} />
+        <Route path='/' element={<Home darkMode={darkMode} />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { HiMoon, HiSun } from 'react-icons/hi';
 
-function Navbar() {
+function Navbar({ darkMode, toggleDarkMode }) {
   return (
-    <section className='flex justify-evenly items-evenly shadow-xl fixed top-0 w-full bg-white z-10 '>
+    <section className={`flex justify-evenly items-evenly shadow-xl fixed top-0 w-full bg-${darkMode ? 'black' : 'white'} text-${darkMode ? 'white' : 'black'} z-10`}>
       <style>
-  @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Montserrat:wght@500&display=swap');
-</style>
-        <div className='align-left my-auto flex'>
-            <h1 className='text-[3rem] ml-[25px] font-[Agbalumo]'>Bharat <span id='samarkand'>Yatra</span></h1>
-        </div>
-        <div className='flex my-auto'>
-            <Link to='/'><li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">Home</a></li></Link>
-            <li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">Places</a></li>
-            <li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">About Us</a></li>
-        </div>
+        @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Montserrat:wght@500&display=swap');
+      </style>
+      <div className='align-left my-auto flex'>
+        <h1 className={`text-[3rem] ml-[25px] font-[Agbalumo] ${darkMode ? 'text-white' : 'text-black'}`}>Bharat <span id='samarkand'>Yatra</span></h1>
+      </div>
+      <div className='flex my-auto'>
+        <Link to='/'><li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">Home</a></li></Link>
+        <li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">Places</a></li>
+        <li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">About Us</a></li>
+      </div>
+      <div className="my-auto">
+        <button className={`m-[10px] text-xl ${darkMode ? 'text-white' : 'text-black'}`} onClick={toggleDarkMode}>
+          {darkMode ? <HiSun style={{ fontSize: '24px' }} /> : <HiMoon style={{ fontSize: '24px' }} />}
+        </button>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
