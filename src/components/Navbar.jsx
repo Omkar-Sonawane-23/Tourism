b236101096c32d16cec385991930e55b229380ef
SnehansuBehera <snehansbehera080903@gmail.com>
2024-05-12 10:50:20 +0530

    Places-section_UI-updated_responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,61 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <section className='flex justify-evenly items-evenly shadow-xl fixed top-0 w-full bg-white z-10 '>
+    <section className='flex justify-evenly items-evenly py-4 sm:py-2 shadow-xl fixed top-0 w-full bg-white z-10 px-4'>
       <style>
-  @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Montserrat:wght@500&display=swap');
-</style>
+        @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Montserrat:wght@500&display=swap');
+      </style>
+
+      <div className='flex flex-stretch justify-between items-center gap-[10vw]'>
         <div className='align-left my-auto flex'>
-            <h1 className='text-[3rem] ml-[25px] font-[Agbalumo]'>Bharat <span id='samarkand'>Yatra</span></h1>
+          <h1 className='text-[2rem] sm:text-[3rem] font-[Agbalumo]'>
+            Bharat <span id='samarkand'>Yatra</span>
+          </h1>
         </div>
-        <div className='flex my-auto'>
-            <Link to='/'><li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">Home</a></li></Link>
-            <li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">Places</a></li>
-            <li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">About Us</a></li>
+
+        <div className='sm:hidden my-auto'>
+          <button className='text-xl' onClick={toggleMenu}>
+            <svg
+              className='w-10 h-10'
+              fill='none'
+              stroke='currentColor'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d={isMenuOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}
+              ></path>
+            </svg>
+          </button>
         </div>
+      </div>
+
+
+
+      <div className={`sm:flex ${isMenuOpen ? 'flex flex-col gap-4 m-4' : 'hidden'} my-auto`}>
+        <Link to='/' className='list-none text-md sm:text-xl hover:text-gray-500 mx-2 sm:mx-4'>
+          Home
+        </Link>
+        <Link to='/places' className='list-none text-md sm:text-xl hover:text-gray-500 mx-2 sm:mx-4'>
+          Places
+        </Link>
+        <Link to='/about' className='list-none text-md sm:text-xl hover:text-gray-500 mx-2 sm:mx-4'>
+          About Us
+        </Link>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
