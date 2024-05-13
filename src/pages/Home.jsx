5f9c1762dbffc5ff6114a6c845ca03f0e2644981
ANKIT MOURYA <ankit.mourya8233@gmail.com>
2024-05-13 15:55:56 +0530

    update the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <section className='mt-[5px]'>
             <img src="https://img.freepik.com/premium-vector/mumbai-skyline-landscape-view-city-mumbai-with-characteristics-buildings-monuments_743272-109.jpg?w=4320" className='w-[100vw] h-[100vh] fixed' alt="" />
             <div className='absolute w-full bottom-[5rem]'>
-                <div className='flex flex-col justify-center items-center mb-[10rem]'>
+                <div className='flex flex-col justify-center items-center mb-[5rem]'>
                     <span className='text-[3rem]'>Welcome,</span><span className='text-[2rem]'> to</span><h1 className='text-[5rem] ml-[25px] font-[Agbalumo]'>Bharat <span id='samarkand'>Yatra</span></h1>
                 </div>
                 <div className='flex justify-center items-center gap-[12rem] h-[5.9rem]'>
