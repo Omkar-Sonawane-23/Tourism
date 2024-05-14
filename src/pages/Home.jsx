69cfd5b18c6dd007705c2daaf68620321b1af2b9
drick-infinity <harshimalviya47487@gmail.com>
2024-05-14 12:06:57 +0530

    home page centered

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,20 +18,20 @@ const Home = () => {
     return (
         <section className='mt-5 flex px-10 py-20 shadow-md ml-10 mr-10'>
             
-            <div>
-                <div className='mr-28'>
+            <div className='w-1/2'>
+                <div className='mr-28 '>
                     <span className='text-[3rem]'>Welcome,</span>
-                    <span className='text-[2rem]'> to</span>
+                    {/* <span className='text-[2rem]'> to</span> */}
                     <div className='animate-bounce'>
                     <h1 className='text-[3rem] font-[Agbalumo]'>Bharat <span id='samarkand'>Yatra</span></h1></div>
                 </div>
-                <div className='w-1/2'>
+                <div className=''>
                     <button type="button" className="text-white bg-[#002d74] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Explore</button>
                     <Link to='/home'><button type="button" className='text-white bg-[#002d74] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Places</button></Link>
                 </div>
             </div>
-            <div className='w-1/2 '>
-            <img src={bg} className='ml-14 rounded-xl shadow-md transform transiton duration-300 hover:scale-105 hover:cursor-pointer' alt="" />
+            <div className='w-1/2 shadow-md py-5 px-5'>
+            <img src={bg} className='shadow-md transform transiton duration-300 hover:scale-105 hover:cursor-pointer' alt="" />
             </div>
         </section>
     )
