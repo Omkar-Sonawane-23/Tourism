c8af5c767d5aae05d4321250fe987422a6067a5f
drick-infinity <harshimalviya47487@gmail.com>
2024-05-14 20:30:25 +0530

    links are also added in navbar

--- src/components/Homeque.jsx
+++ src/components/Homeque.jsx
@@ -10,23 +10,26 @@ const Homeque = () => {
   };
 
   return (
-    <section className='grid grid-cols-3 gap-4'>
+    <section className='max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16 grid gird-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-10 '>
       {data.map((ele, i) => (
-        <div key={i} className='w-[20rem] h-[25rem] m-[5rem] relative flex flex-col justify-center items-center'>
-          <div className='w-[20rem] h-[30rem] overflow-hidden rounded-[30px]'>
-            <img src={data[i].image} className='rounded-[30px] w-[25rem] h-[30rem] bg-cover' alt="Heritage Image" />
-          </div>
-          <div className='w-[20rem] h-[20rem] bg-[black] mt-[-30rem] z-[2] opacity-[30%] rounded-[30px] h-[20rem] flex flex-col justify-center items-center'>
-            <h1 className='text-[4rem] mt-[0px] text-center leading-[60px] z-[2] text-[white]'>{data[i].name}</h1>
-            <p className='text-[white] z-[2] w-[18rem]'>{data[i].description}</p>
-          </div>
+        <div key={i} className='px-4 shadow dark:bg-gray-800 dark:border-gray-700'>
+          <div className=' bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between h-full'>
+            <img src={data[i].image} className='transform transiton duration-300 hover:scale-105 hover:cursor-pointer' alt="Heritage Image" />
+          
+            <h1 className='font-bold text-lg flex items-center mt-2'>{data[i].name}</h1>
+            <p className='text-gray-900 mb-2 hover:text-indigo-600 inline-block'>{data[i].description}</p>
+          <div className='flex items-center'>
             <Link to={`/detail/?id=${i}`} key={i}>
-              <button onClick={() => showdetail(i)} className='w-[80px] h-[30px] border-2 rounded-xl text-[black] backdrop-blur hover:text-[1.25rem] cursor-pointer text-[white] hover:w-[100px] hover:h-[50px] duration-[0.4s] mt-[5rem]'>Explore</button>
-            </Link>
+              <button onClick={() => showdetail(i)} className='text-white bg-[#002d74] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Explore</button>
+            </Link></div>
+          </div>
         </div>
+          
 
       ))}
+      
     </section>
+    
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,11 @@ function Navbar() {
             <h1 className='text-[3rem] ml-[25px] font-[Agbalumo]'>Bharat <span id='samarkand'>Yatra</span></h1>
         </div>
         <div className='flex my-auto'>
-            <Link to='/'><li className='list-none text-xl text-[#002d74] hover:text-blue-500 font-bold m-[10px]'><a href="">Home</a></li></Link>
-            <li className='list-none text-xl text-[#002d74]  hover:text-blue-500 font-bold m-[10px]'><a href="">Places</a></li>
-            <li className='list-none text-xl text-[#002d74] m-[10px]  hover:text-blue-500 font-bold'><a href="">About Us</a></li>
-            <li className='list-none text-xl text-white bg-[#002d74] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'><a href="/login">Login </a></li>
-            <li className='list-none text-xl text-white bg-[#002d74] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'><a href="/signup">Sign up </a></li>
+            <Link to='/' className='list-none text-xl text-[#002d74] hover:text-blue-500 font-bold m-[10px]'>Home</Link>
+            <Link to='/' className='list-none text-xl text-[#002d74]  hover:text-blue-500 font-bold m-[10px]'>Places</Link>
+            <Link to='/' className='list-none text-xl text-[#002d74] m-[10px]  hover:text-blue-500 font-bold'>About Us</Link>
+            <Link to='/login'className='list-none text-xl text-white bg-[#002d74] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Login</Link>
+            <Link to='/signup' className='list-none text-xl text-white bg-[#002d74] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Sign up</Link>
         </div>
     </section>
   )
