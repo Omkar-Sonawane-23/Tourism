26aecc4344dbc8faf8e1c86e0b62e958b59fddaf
harshmishra19 <harshmishra7649@gmail.com>
2024-05-14 09:32:50 +0530

    Added icons on the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,15 @@ function Navbar() {
     <section className='flex justify-evenly items-evenly shadow-xl fixed top-0 w-full bg-white z-10 '>
       <style>
   @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Montserrat:wght@500&display=swap');
+  @import url('https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css');
 </style>
         <div className='align-left my-auto flex'>
             <h1 className='text-[3rem] ml-[25px] font-[Agbalumo]'>Bharat <span id='samarkand'>Yatra</span></h1>
         </div>
         <div className='flex my-auto'>
-            <Link to='/'><li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">Home</a></li></Link>
-            <li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">Places</a></li>
-            <li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">About Us</a></li>
+            <Link to='/'><li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href=""><i class="ri-home-fill"></i> Home</a></li></Link>
+            <li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href=""><i class="ri-landscape-fill"></i> Places</a></li>
+            <li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href=""><i class="ri-customer-service-2-fill"></i> About Us</a></li>
         </div>
     </section>
   )
