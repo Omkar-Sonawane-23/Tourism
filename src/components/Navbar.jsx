4b03ba3c3280e4c7f6c1249585b8a60226f30c15
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-05-14 20:15:01 +0530

    added-redirect-functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,10 @@ function Navbar() {
       <style>
   @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Montserrat:wght@500&display=swap');
 </style>
-        <div className='align-left my-auto flex'>
+      
+        <Link to='/' className='align-left my-auto flex'>
             <h1 className='text-[3rem] ml-[25px] font-[Agbalumo]'>Bharat <span id='samarkand'>Yatra</span></h1>
-        </div>
+        </Link>
         <div className='flex my-auto'>
             <Link to='/'><li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">Home</a></li></Link>
             <li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">Places</a></li>
