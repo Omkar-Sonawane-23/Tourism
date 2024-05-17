c37f00e29e4726518d6d845c56b03c9b09c0337f
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-05-17 19:44:58 +0530

    Issue269-Adding Pagination

--- src/components/Homeque.jsx
+++ src/components/Homeque.jsx
@@ -1,56 +1,66 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Data from "../../Data/Data.json";
+import Pagination from "./Pagination";
 
 const Homeque = () => {
-  const data = Data.destinations;
+  // const data = Data.destinations;
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [currentData, setCurrentData] = useState([]);
+  const [pageSum, setPageSum] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % 3);
     }, 3000);
 
-    return () => clearInterval(intervalId); 
+    return () => clearInterval(intervalId);
   }, [3]);
-  
+
   const showdetail = (index) => {
     console.log(index);
   };
 
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 gap-y-2 justify-center items-center">
-      {data.map((destination, i) => (
-        <article className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto mt-24 bg-gray-900/40 shadow-lg hover:shadow-2xl transition duration-500 ease-in-out transform"
-         key={i}>
+    <>
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 gap-y-2 justify-center items-center">
+        {currentData.map((destination, i) => (
+          <article
+            className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto mt-24 bg-gray-900/40 shadow-lg hover:shadow-2xl transition duration-500 ease-in-out transform"
+            key={i}
+          >
             <img
               src={destination.image[currentImageIndex]}
               className="absolute inset-0 h-full w-full object-cover"
               alt={destination.name}
             />
-          <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-          <h3 className="z-10 mt-3 text-4xl  font-bold text-white text-center">
-            {data[i].name}
-          </h3>
-          <div className="z-10 gap-y-1 pt-4 overflow-hidden text-center text-sm leading-6 text-gray-300">
-            {data[i].description}
-          </div>
+            <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+            <h3 className="z-10 mt-3 text-4xl  font-bold text-white text-center">
+              {destination.name}
+            </h3>
+            <div className="z-10 gap-y-1 pt-4 overflow-hidden text-center text-sm leading-6 text-gray-300">
+              {destination.description}
+            </div>
 
-          <div className="z-10 mt-4 justify-center text-center items-center">
-          <Link to={`/detail/?id=${i}`} key={i}>
-            <button
-              onClick={() => showdetail(i)}
-              className="w-[80px] h-[30px]  border-2 rounded-xl text-[white] backdrop-blur  cursor-pointer text-[white]  duration-[0.4s] mt-[5rem] hover:bg-[#f9a826] hover:text-[#1f2937] hover:shadow-lg hover:scale-105 transition duration-500 ease-in-out transform
+            <div className="z-10 mt-4 justify-center text-center items-center">
+              <Link to={`/detail/?id=${pageSum + i}`} key={pageSum + i}>
+                <button
+                  onClick={() => showdetail(pageSum + i)}
+                  className="w-[80px] h-[30px]  border-2 rounded-xl text-[white] backdrop-blur  cursor-pointer text-[white]  duration-[0.4s] mt-[5rem] hover:bg-[#f9a826] hover:text-[#1f2937] hover:shadow-lg hover:scale-105 transition duration-500 ease-in-out transform
               "
-            >
-              Explore
-            </button>
-            
-          </Link>
-        </div>
-       </article>
-      ))}
-    </section>
+                >
+                  Explore
+                </button>
+              </Link>
+            </div>
+          </article>
+        ))}
+      </section>
+
+      <div>
+        <Pagination setCurrentData={setCurrentData} setPageSum={setPageSum} />
+      </div>
+    </>
   );
 };
 
