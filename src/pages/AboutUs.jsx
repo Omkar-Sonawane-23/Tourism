e42bb27fdbec077a1de250e4b85e9575b69b100b
Hack2Health <hemashree21.msh@gmail.com>
2024-05-17 16:27:41 +0530

    Updated About Us page
    Signed-off-by: Hemashree M S <hemashree21.msh@gmail.com>

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -21,14 +21,14 @@ const AboutUs = () => {
     <div className='about-us-block'>
         <div className='about-us-community' onMouseOver={()=>setExploreHover(true)} onMouseLeave={()=>setExploreHover(false)}>
             <div style={{display: 'flex', alignItems: 'center', gap: '1rem'}}>
-                <img src={exploreHover ? explorehover : explore} alt="" style={{marginLeft: '1rem', marginTop: '1rem', width: '4rem'}}/>
+                <img src={exploreHover ? explorehover : explore} alt="" style={{marginLeft: '1rem', marginTop: '1rem', width: '3rem'}}/>
                 <div className='about-us-blk-text'>Discover India</div>
             </div>
             <p className='about-us-blk-para'>If you’re looking for popular tourist spots or hidden gems, BharatYatra offers a curated list of places that cater to all interests. With the ability to search and filter destinations, you can easily plan your travel and ensure you experience the rich cultural, magnificient architecture and natural tapestry that India has to offer.</p>
         </div>
         <div className='about-us-product' onMouseOver={()=>setActivityHover(true)} onMouseLeave={()=>setActivityHover(false)}>
             <div style={{display: 'flex', alignItems: 'center', gap: '1rem'}}>
-                <img src={activityHover ? activityhover : activity} alt="" style={{marginLeft: '1rem', marginTop: '1rem', width: '4rem'}}/>
+                <img src={activityHover ? activityhover : activity} alt="" style={{marginLeft: '1rem', marginTop: '1rem', width: '3rem'}}/>
                 <div className='about-us-blk-text'>Activity Highlights</div>
             </div>
             <p className='about-us-blk-para'>Whether you’re into trekking in the Himalayas, diving in the Andaman Islands, or practicing yoga in Rishikesh, BharatYatra provides detailed information on the best activities to try. You can discover new adventures, learn about local traditions, and find relaxation spots, all tailored to enhance your travel experiences. </p>
@@ -37,14 +37,14 @@ const AboutUs = () => {
     <div className='about-us-block'>
         <div className='about-us-location' onMouseOver={()=>setEventHover(true)} onMouseLeave={()=>setEventHover(false)}>
             <div style={{display: 'flex', alignItems: 'center', gap: '1rem'}}>
-                <img src={eventHover ? eventhover : event} alt="" style={{marginLeft: '1rem', marginTop: '1rem', width: '4rem'}}/>
+                <img src={eventHover ? eventhover : event} alt="" style={{marginLeft: '1rem', marginTop: '1rem', width: '3rem'}}/>
                 <div className='about-us-blk-text'>Major Events</div>
             </div>
             <p className='about-us-blk-para'>From vibrant festivals like Diwali and Holi to cultural exhibitions, music concerts, and local fairs, we ensur that travelers don't miss out on unique experiences. By participating in these events, you can immerse themselves in local culture, witness traditional celebrations, and create unforgettable memories during your journey.</p>
         </div>
         <div className='about-us-event' onMouseOver={()=>setSeasonHover(true)} onMouseLeave={()=>setSeasonHover(false)}>
             <div style={{display: 'flex', alignItems: 'center', gap: '1rem'}}>
-                <img src={seasonHover ? season : seasonhover} alt="" style={{marginLeft: '1rem', marginTop: '1rem', width: '4rem'}}/>
+                <img src={seasonHover ? season : seasonhover} alt="" style={{marginLeft: '1rem', marginTop: '1rem', width: '3rem'}}/>
                 <div className='about-us-blk-text'>Ideal Season</div>
             </div>
             <p className='about-us-blk-para'>Detailed timelines suggest the perfect months to visit each place, factoring in climate conditions and peak tourist seasons. Additionally, it informs you about specific events and festivals occurring throughout the year, allowing you to plan your trips to coincide with these vibrant celebrations, enhancing your overall travel experience.</p>
