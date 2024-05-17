3f2ca01f26a6818a16903a1dd88b100e4e610eaa
Priyamvada Shah <118747750+PriyamvadaShah@users.noreply.github.com>
2024-05-17 17:47:39 +0530

    Tourism Friendly Loader. (#268)
    * Update App.css
    
    * Update App.jsx
    
    * Create LoaderData.js
    
    * Update Home.jsx
    
    * Create Loader.jsx
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Sameer <134498717+sameer-soni@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import Explore from "./pages/Explore";
 import Homeque from "./components/Homeque";
 import DetailView from "./components/DetailView";
 import NotFound from "./pages/NotFound";
+import ContactUs from "./pages/ContactUs";
 import "./App.css";
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
         <Route path="/home" element={<Homeque />} />
         <Route path="/explore" element={<Explore />} />
@@ -20,7 +20,7 @@ function App() {
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
-        {/* <Route path="/contact" element={<ContactUs />} /> */}
+        <Route path="/contact" element={<ContactUs />} />
       </Routes>
     </BrowserRouter>
   );
