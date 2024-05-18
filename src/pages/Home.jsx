6128c57f7c9fe1ab3af38e1055f16cab1b4f7425
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-05-18 14:26:11 +0530

    Navbar Responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,14 +30,14 @@ const Home = () => {
             alt=""
           />
           <div className="absolute w-full bottom-[5rem]">
-            <div className="flex flex-col justify-center items-center mb-[5rem]">
+            <div className="flex flex-col justify-center items-center sm:mb-[5rem] mb-[0rem]">
               <span className="text-[3rem]">Welcome,</span>
               <span className="text-[2rem]"> to</span>
-              <h1 className="text-[5rem] ml-[25px] font-[Agbalumo]">
+              <h1 className="text-[5rem] sm:ml-[25px] text-center font-[Agbalumo]">
                 Bharat <span id="samarkand">Yatra</span>
               </h1>
             </div>
-            <div className="flex justify-center items-center gap-[12rem] h-[5.9rem]">
+            <div className="flex justify-center items-center sm:gap-[12rem] sm:h-[5.9rem] h-[11rem] sm:flex-row flex-col gap-[20px] ">
               <Link to="/explore">
                 <button
                   className="text-[25px] w-[10rem] h-[2.9rem] rounded-xl bg-[black] text-[white] hover:text-[green] duration-[0.4s]"
