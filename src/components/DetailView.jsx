c84ee328df2ecb5ae65e56201ad0bbb49514755d
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-05-18 15:44:01 +0530

    updating files

--- src/components/DetailView.jsx
+++ src/components/DetailView.jsx
@@ -21,38 +21,38 @@ const DetailView = () => {
   return (
     <section className="flex flex-col mt-20 items-center justify-center w-full h-full">
       {data && (
-        <div className="flex flex-col items-center  justify-center w-4/5 h-full">
+        <div className="flex flex-col items-center  justify-center w-11/12 h-full">
           <img src={data.image} alt={data.title} />
           <h1 className="text-3xl font-bold mt-5">{data.location}</h1>
-          <p className="text-lg mt-5 text-justify">{data.description}</p>
+          <p className="text-lg sm:text-lg md:text-xl mt-5 text-justify">{data.description}</p>
 
-          <div className="grid sm:grid-cols-2 md:grid-cols-4  gap-2 gap-y-4 mt-5 w-full"
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4  gap-5 gap-y-4 mt-5 w-full"
           style={{alignItems:'flex-start'}}
           >
-            <div className="flex flex-col items-center justify-center">
-              <h2 className="text-xl font-bold">Category</h2>
-              <p className="text-lg mt-2">{data.category}</p>
+            <div className="flex flex-col p-4">
+              <h2 className="text-lg sm:text-lg md:text-xl font-bold">Category</h2>
+              <p className="text-lg sm:text-lg md:text-xl mt-2">{data.category}</p>
             </div>
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col  p-4">
               <h2 className="text-xl font-bold">Best time to visit</h2>
-              <p className="text-lg mt-2">{data.bestTimeToVisit}</p>
+              <p className="text-lg sm:text-lg md:text-xl mt-2">{data.bestTimeToVisit}</p>
             </div>
-            <div className="flex flex-col items-center justify-center ">
+            <div className="flex flex-col  p-4">
               <h2 className="text-xl font-bold">Popular activities</h2>
               <ul className="list-disc text-justify">
               {data.popularActivities.map((activity, index) => (
-                <li key={index} className="text-lg mt-2">
+                <li key={index} className="text-lg sm:text-lg md:text-xl mt-2">
                   {activity}
                 </li>
               ))}
               </ul>
             </div>
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col  p-4">
               <h2 className="text-xl font-bold">Nearby attractions</h2>
               <ul className="list-disc text-justify">
 
               {data.nearbyAttractions.map((attraction, index) => (
-                <li key={index} className="text-lg mt-2">
+                <li key={index} className="text-lg sm:text-lg md:text-xl mt-2">
                   {attraction}
                 </li>
               ))}
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -18,7 +18,7 @@ const Details = () => {
       <h1 className="text-3xl sm:text-4xl mb-8 text-center font-bold text-gray-800">
         Discover Latest Events
       </h1>
-      <section className="text-gray-600 body-font w-4/5 ">
+      <section className="text-gray-600 body-font w-11/12 ">
         <div className="">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             {eventDetails.map((event, index) => (
@@ -68,7 +68,7 @@ const Details = () => {
               Travel Tips:
             </h1>
             <ul className=" list-disc ml-8 text-lg text-gray-700">
-              {data[id].travelTips.map((tip, index) => (
+              {data[id]?.travelTips.map((tip, index) => (
                   <li key={index} className="text-lg text-gray-700">{tip}</li>
                 
               ))}
