d20f2ce769ae2ef69ff1a70d139a18365ce62846
Harsh Ninawe <harshninawe520@gmail.com>
2024-05-19 19:27:45 +0530

    new clean code remove unwanted comment

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -93,35 +93,6 @@ const Register = () => {
               </div>
             </form>
           </div>
-
-          {/*               
-                <div className='auth-google'>
-                    <img src={Google} alt="" className='sm-icon'/>Sign Up with Google
-                </div>
-                <div style={{display: 'flex', marginTop: '15px',justifyContent: 'center', alignItems: 'center', width: '90%'}}>
-                <div className='connect-line'></div>
-                <span className='connect'>Or register with</span>
-                <div className='connect-line'></div>
-            </div>
-            <div className='form-auth'>
-                <form action="">
-                    <input type='text' placeholder='Username' className='auth-email' required/>
-                    <input type='email' placeholder='Email' className='auth-email' required/>
-                    <input type={passwordVisible ? 'text' : 'password'} placeholder='Password' className='auth-email' onClick={togglePasswordVisibility} required/>
-                    <i className="eye-icon" onClick={togglePasswordVisibility}>
-                    <img src={passwordVisible ? show : eye} alt="" />
-                    </i>
-                <div style={{marginTop: '10px', display: 'flex', justifyContent: 'space-between', verticalAlign: 'middle'}}>
-                  <label className="custom-checkbox">
-                    <input type="checkbox" className='remember-checkbox'/>
-                    <span className="checkmark"></span><span className='remember-me'>Remember me</span>
-                  </label>
-                  </div>
-                </form>
-                <button type='submit' className='auth-continue'>Sign Up</button>
-            </div>
-            <div className='registered'>Already registered? <span style={{fontWeight: 'bold', cursor: 'pointer'}}><Link to='/login'>Login</Link></span>
-  </div>*/}
         </div>
       </div>
     </div>
