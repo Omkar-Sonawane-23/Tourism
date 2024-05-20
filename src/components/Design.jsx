e3f3e608af3c7b8f2c52afea524a046a46f49cfe
void_name_zayed <reachzayed@proton.me>
2024-05-20 19:03:19 +0530

    fixed + - issue in accordion

--- src/components/Design.jsx
+++ src/components/Design.jsx
@@ -7,11 +7,15 @@ export const Design = ({question,answer}) => {
   function one(){
     setShow(!show);
   }
+  function flipSign(){
+    sign==="+"?setSign("-"):setSign("+");
+    setShow(!show);
+  }
   return (
     <div  className="card" style={{Width:"50%",marginBottom:"20px"}}>
       <div class="topic" style={{display:"flex",justifyContent:"space-between"}}>
       <h2 style={{color:"black",width:"100%",paddingLeft:"10px",fontSize:"20px",cursor:"pointer"}} onClick={()=>{one()}}>{"Q."+question}</h2>
-      <p style={{color:"black",paddingRight:"10px",paddingLeft:"30px"}}>{show?"-":"+"}</p>
+      <p style={{color:"black",paddingRight:"10px",paddingLeft:"30px", cursor:"pointer"}} onClick={()=>{flipSign()}}>{sign}</p>
       </div>
       {
         show && <p style={{color:"black",background:"white",border:"1px solid black",padding:"20px"}}>{answer}</p>
