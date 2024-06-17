af3d947fa1f8405101df1c90587326e15a0e6fbf
pawarkaustubh <pawarkaustubh2906@gmail.com>
2024-06-17 13:55:06 +0530

    Add Shadow Effect #511

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -166,12 +166,15 @@ const Explore = () => {
         {filteredPlaces.map((place, index) => (
           <article
             key={index}
-            className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto mt-24 bg-gray-900/40 shadow-lg hover:shadow-2xl transition duration-500 ease-in-out transform"
+            className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto mt-24 bg-gray-900/40 shadow-lg hover:shadow-3xl transition duration-500 ease-in-out transform"
+            style={{
+              boxShadow: '0 8px 30px rgba(0, 0, 0, 1.0)',
+            }}
           >
             <img
               src={place.image}
               alt=""
-              className="absolute inset-0 h-full w-full object-cover"
+              className="absolute inset-0 h-full w-full object-cover "
             />
             <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
             <h3 className="z-10 mt-3 text-4xl  font-bold text-white text-center">
